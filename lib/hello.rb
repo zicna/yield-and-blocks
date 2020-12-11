4dd455cf9a0649dee286cb4c98e1dfb83b066a87
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-10 22:15:40 -0600

    commit tree 0257a363bc58ba30db319eac92195fa5fc000e72

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,31 @@
-def hello_t
-
-end
+# def hello_t(array)
+#     i = 0
+#     while i < array.length
+#         yield(array[i])
+#         i = i + 1
+#     end
+#     array
+# end
 
 # call your method here!
 
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#     if name.start_with?("T")
+#         puts "Hi, #{name}"
+#     end
+# end
+
+def hello_t(array)
+    if block_given?
+      i = 0
+  
+      while i < array.length
+        yield(array[i])
+        i = i + 1
+      end
+  
+      array
+    else
+      puts "Hey! No block was given!"
+    end
+  end 
